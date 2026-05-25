d77e4febae971f180f3f533e5e4733a53e1103c3
amanueltem <amanueltemesgen62@gmail.com>
2026-05-25 09:49:05 +0300

    project initialized.

---
+++ src/main/java/com/amman/fidel_ocr_engine/FidelOcrEngineApplication.java
@@ -0,0 +1,13 @@
+package com.amman.fidel_ocr_engine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FidelOcrEngineApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(FidelOcrEngineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/amman/fidel_ocr_engine/FidelOcrEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amman.fidel_ocr_engine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FidelOcrEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
